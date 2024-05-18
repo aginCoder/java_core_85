eb5096ac68c5fcf93e399783fedfd1a57b02dd0c
aginCoder <phamgiaan545@gmail.com>
2024-05-18 22:16:25 +0700

    assignment06

--- src/main/java/Program04.java
+++
@@ -1,9 +0,0 @@
-public class Program04 {
-    public static void main(String[] args) {
-        Student student = new Student();
-        student.setScore(5.0);
-        student.showInfo();
-        student.plusScore(2.0);
-        student.showInfo();
-    }
-}
