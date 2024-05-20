006ea90c68737439658aadea8ac7df2c1db1a4e7
aginCoder <phamgiaan545@gmail.com>
2024-05-20 19:17:09 +0700

    lesson08

--- src/main/java/GioiTinh.java
+++
@@ -1,3 +0,0 @@
-public enum GioiTinh {
-    NAM, NU, KHAC
-}
--- src/main/java/Program06.java
+++
@@ -1,6 +0,0 @@
-public class Program06 {
-    public static void main(String[] args) {
-        VietnamesePhone vietnamesePhone = new VietnamesePhone();
-        vietnamesePhone.showMenu();
-    }
-}
