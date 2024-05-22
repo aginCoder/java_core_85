6ef8d1e145b0915699e5d8f3f3dc35e86d342b30
aginCoder <phamgiaan545@gmail.com>
2024-05-22 21:46:48 +0700

    lesson09

--- src/main/java/FileDemo.java
+++ src/main/java/FileDemo.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class FileDemo {
+import java.io.File;
+import java.io.IOException;
+
+public class FileDemo {
+    public static void main(String[] args) throws IOException {
+//        String path = "src/main/resources/abc.txt";
+//
+//        boolean createNewFile = FileManager.createFile(path);
+//        System.out.println("createNewFile = " + createNewFile);
+//
+//        boolean delete = FileManager.delete("D:\\VTI\\abc.txt");
+//        System.out.println("delete = " + delete);
+//
+//        String[] names = FileManager.list("C:\\");
+//        for (String name : names) {
+//            System.out.println("name = " + name);
+//        }
+
+        boolean mkdirs =  FileManager.mkdirs("D:\\VTI\\ABC");
+        System.out.println("mkdirs = " + mkdirs);
+    }
 }
